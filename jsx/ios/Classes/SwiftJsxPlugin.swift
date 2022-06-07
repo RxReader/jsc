b4eb616a1312e1f2fbc79553c6fb9d58e89fbde9
v7lin <v7lin@qq.com>
2022-06-07 16:38:09 +0800

    format

--- jsx/ios/Classes/SwiftJsxPlugin.swift
+++ jsx/ios/Classes/SwiftJsxPlugin.swift
@@ -2,17 +2,17 @@ import Flutter
 import UIKit
 
 public class SwiftJsxPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "v7lin.github.io/jsx", binaryMessenger: registrar.messenger())
-    let instance = SwiftJsxPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-      if ("doesnt_matter" == call.method) {
-          result(nil);
-      } else {
-          result(FlutterMethodNotImplemented)
-      }
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "v7lin.github.io/jsx", binaryMessenger: registrar.messenger())
+        let instance = SwiftJsxPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if ("doesnt_matter" == call.method) {
+            result(nil);
+        } else {
+            result(FlutterMethodNotImplemented)
+        }
+    }
 }
